cc490a394088bb58841f6d13401276d3729ee94e
zpj <330273931@qq.com>
2017-11-28 16:49:23 +0800

    安装小案例

---
+++ ZfbGridView/app/src/main/java/com/example/administrator/zfbgridview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.administrator.zfbgridview;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * @Description:主页
+ * @author http://blog.csdn.net/finddreams
+ */ 
+public class MainActivity extends Activity{
+	private MyGridView gridview;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initView();
+	}
+
+	private void initView() {
+		gridview=(MyGridView) findViewById(R.id.gridview);
+		gridview.setAdapter(new MyGridAdapter(this));
+		
+	}
+}
